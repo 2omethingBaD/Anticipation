5369dd6dcadfcbe92eae1818df8c815fb0708283
2omethingBaD <113650499+2omethingBaD@users.noreply.github.com>
2023-03-06 17:54:12 -0500

    movement bounds fix

--- playerController.cs
+++ playerController.cs
@@ -13,49 +13,72 @@ public class playerController : MonoBehaviour
     public bool _isAllowedRIGHT;
 
 
+    void Awake()
+    {
+        _isAllowedUP = false;
+        _isAllowedDOWN = true;
+        _isAllowedLEFT = true;
+        _isAllowedRIGHT = true;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>(); //gets rigid body so you dont have to type whole thing
         _rb.gravityScale = 0; //stops player from falling until set to at least 1
+    }
+
+
+    //called to allow player to move again
+    private void OnCollisionExit2D(Collision2D collision)
+    {
         _isAllowedUP = true;
         _isAllowedDOWN = true;
         _isAllowedLEFT = true;
         _isAllowedRIGHT = true;
-
     }
 
 
-    //called when player touches the bounds
+    //called to stop player form bouncing on bounds
     void OnCollisionEnter2D(Collision2D collision)
     {
-        while(collision.gameObject.name == "bounds" & Input.GetKey(KeyCode.W))
+        if(collision.gameObject.name == "BOUNDS" && Input.GetKey(KeyCode.W))
         {
             _isAllowedUP = false;
-            Debug.Log("hi");
         }
-        
+        if (collision.gameObject.name == "BOUNDS" && Input.GetKey(KeyCode.A))
+        {
+            _isAllowedLEFT = false;
+        }
+        if (collision.gameObject.name == "BOUNDS" && Input.GetKey(KeyCode.S))
+        {
+            _isAllowedDOWN = false;
+        }
+        if (collision.gameObject.name == "BOUNDS" && Input.GetKey(KeyCode.D))
+        {
+            _isAllowedRIGHT = false;
+        }
     }
 
-
+    
     // Update is called once per frame
     void Update()
     {
-        
         //player movement
-        if (Input.GetKey(KeyCode.W) & _isAllowedUP == true)
+        if (Input.GetKey(KeyCode.W) && _isAllowedUP == true)
         {
             transform.position += Vector3.up * playerSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && _isAllowedLEFT == true)
         {
             transform.position += Vector3.left * playerSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && _isAllowedDOWN == true)
         {
             transform.position += Vector3.down * playerSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && _isAllowedRIGHT == true)
         {
             transform.position += Vector3.right * playerSpeed * Time.deltaTime;
         }
